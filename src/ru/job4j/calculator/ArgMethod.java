460ede808661c072098439b0e9eabc24991b4585
Zhv <oleg@openres.ru>
2021-05-21 17:25:12 +0700

    4.1. Аргументы [#295521 #159021]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class ArgMethod {
 
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + " Age:" + age);
     }
 
     public static void main(String[] args) {
