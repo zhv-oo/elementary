301c1f97b156f10d72f73b654b48efbb427acd40
Zhv <oleg@openres.ru>
2021-05-22 20:55:42 +0700

    6. Идеальный вес [#41584 #159420]

--- src/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/calculator/Fit.java
@@ -2,13 +2,11 @@ package ru.job4j.calculator;
 
 public class Fit {
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
