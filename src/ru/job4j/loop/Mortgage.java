634f12ab05466f668488aa46a09c63eb03b592bc
zhv-oo <zhitenev.oleg@yandex.ru>
2021-05-26 19:34:20 +0700

    5.7. Ипотека [#156315 #160909]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double am = amount;
         while (amount > 0) {
-            amount = amount + (int) (am * (percent / 100));
+            amount = amount + (int) (amount * (percent / 100));
             amount = amount - salary;
             year++;
         }
